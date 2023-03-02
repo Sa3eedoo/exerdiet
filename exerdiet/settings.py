845c88156d69cf5b9cf2fb2ce1c7223830781c0b
Mahmoud Saeed <mahmoudsaid246@gmail.com>
2023-03-02 18:53:59 +0200

    delete pycache

--- exerdiet/settings.py
+++ exerdiet/settings.py
@@ -83,8 +83,11 @@ WSGI_APPLICATION = 'exerdiet.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'exerdiet',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'RrRSs@$5'
     }
 }
 
