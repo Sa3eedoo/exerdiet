d1370fd6e9bd02c5550689605276f5fb1814eacd
Mahmoud Saeed <mahmoudsaid246@gmail.com>
2023-03-06 00:24:55 +0200

    extending the user model

--- exerdiet/settings.py
+++ exerdiet/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'debug_toolbar',
-    'core',
     'exercise',
-    'diet'
+    'diet',
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -132,3 +132,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
