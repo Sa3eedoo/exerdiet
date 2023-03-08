ff061feed7677da03ca6346703c43be953b592d4
Mahmoud Saeed <mahmoudsaid246@gmail.com>
2023-03-09 00:53:08 +0200

    core models v1.1

--- core/models.py
+++ core/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.core.validators import MaxValueValidator
 
 
 class User(AbstractUser):
@@ -8,56 +7,36 @@ class User(AbstractUser):
 
 
 class Trainee(models.Model):
-    ACTIVITY_LEVEL_HIGH = 'H'
-    ACTIVITY_LEVEL_MEDIUM = 'M'
-    ACTIVITY_LEVEL_LOW = 'L'
-    ACTIVITY_LEVEL_CHOICES = [
-        (ACTIVITY_LEVEL_HIGH, 'High'),
-        (ACTIVITY_LEVEL_MEDIUM, 'Medium'),
-        (ACTIVITY_LEVEL_LOW, 'Low')
-    ]
+    class ActivityLevel(models.TextChoices):
+        HIGH = 'H', 'High'
+        MEDIUM = 'M', 'Medium'
+        LOW = 'L', 'Low'
 
-    GOAL_GAIN = 'G'
-    GOAL_KEEP = 'K'
-    GOAL_LOSE = 'L'
-    GOAL_CHOICES = [
-        (GOAL_GAIN, 'Gain'),
-        (GOAL_KEEP, 'Keep'),
-        (GOAL_LOSE, 'Lose')
-    ]
+    class Goal(models.TextChoices):
+        GAIN = 'G', 'Gain'
+        KEEP = 'K', 'Keep'
+        LOSE = 'L', 'Lose'
 
     birthdate = models.DateField()
-    height = models.PositiveSmallIntegerField(
-        validators=[MaxValueValidator(400)]
-    )
-    weight = models.PositiveSmallIntegerField(
-        validators=[MaxValueValidator(800)]
-    )
-    calories_needs = models.PositiveIntegerField()
-    calories_intake = models.PositiveIntegerField()
-    calories_burned = models.PositiveIntegerField()
-    water_intake = models.PositiveIntegerField(
-        validators=[MaxValueValidator(10)]
-    )
-    water_needs = models.PositiveIntegerField(
-        validators=[MaxValueValidator(10)]
-    )
-    carbs_ratio = models.PositiveSmallIntegerField(
-        validators=[MaxValueValidator(100)]
-    )
-    fats_ratio = models.PositiveSmallIntegerField(
-        validators=[MaxValueValidator(100)]
-    )
-    protein_ratio = models.PositiveSmallIntegerField(
-        validators=[MaxValueValidator(100)]
-    )
-    was_active_today = models.BooleanField()
-    streak = models.PositiveIntegerField()
+    height = models.DecimalField(max_digits=3, decimal_places=1)
+    weight = models.DecimalField(max_digits=3, decimal_places=1)
+    daily_calories_needs = models.DecimalField(max_digits=5, decimal_places=1)
+    daily_calories_intake = models.DecimalField(max_digits=5, decimal_places=1)
+    daily_calories_burned = models.DecimalField(max_digits=5, decimal_places=1)
+    daily_water_intake = models.DecimalField(max_digits=5, decimal_places=1)
+    daily_water_needs = models.DecimalField(max_digits=5, decimal_places=1)
+    carbs_ratio = models.DecimalField(max_digits=2, decimal_places=2)
+    fats_ratio = models.DecimalField(max_digits=2, decimal_places=2)
+    protein_ratio = models.DecimalField(max_digits=2, decimal_places=2)
+    was_active_today = models.BooleanField(default=False)
+    daily_streak = models.PositiveSmallIntegerField(default=0)
     activity_level = models.CharField(max_length=1,
-                                      choices=ACTIVITY_LEVEL_CHOICES,
-                                      default=ACTIVITY_LEVEL_MEDIUM)
+                                      choices=ActivityLevel.choices,
+                                      default=ActivityLevel.MEDIUM)
     goal = models.CharField(max_length=1,
-                            choices=GOAL_CHOICES,
-                            default=GOAL_KEEP)
-    image = models.ImageField(upload_to='core/images/trainees')
+                            choices=Goal.choices,
+                            default=Goal.KEEP)
+    image = models.ImageField(
+        upload_to='core/images/trainees', null=True, blank=True
+    )
     user = models.OneToOneField(User, on_delete=models.CASCADE)
