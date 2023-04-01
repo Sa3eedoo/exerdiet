7254f5b67eba77611965ba87c06f4f29aea013f9
Mahmoud Saeed <mahmoudsaid246@gmail.com>
2023-04-01 03:16:00 +0200

    core admin v2

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,15 @@
+from datetime import date
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.urls import reverse
 from django.utils.html import format_html
 from . import models
+
+CALORIE_LEVEL_LOW = 1800
+CALORIE_LEVEL_HIGH = 2200
+AGE_TEEN = 12
+AGE_ADULT = 20
+AGE_OLD = 60
 
 
 class TraineeStatusFilter(admin.SimpleListFilter):
@@ -95,8 +102,128 @@ class UserAdmin(BaseUserAdmin):
         self.message_user(request, message)
 
 
+class CalorieLevelFilter(admin.SimpleListFilter):
+    title = 'calorie level'
+    parameter_name = 'calorie_level'
+    FILTER_LOW = f'<{CALORIE_LEVEL_LOW}'
+    FILTER_MED = f'<{CALORIE_LEVEL_HIGH}'
+    FILTER_HIGH = f'>{CALORIE_LEVEL_HIGH}'
+
+    def lookups(self, request, model_admin):
+        return [
+            (self.FILTER_LOW, 'Low'),
+            (self.FILTER_MED, 'Medium'),
+            (self.FILTER_HIGH, 'High')
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == self.FILTER_LOW:
+            return queryset.filter(daily_calories_needs__gt=0).filter(daily_calories_needs__lt=CALORIE_LEVEL_LOW)
+        if self.value() == self.FILTER_MED:
+            return queryset.filter(daily_calories_needs__gte=CALORIE_LEVEL_LOW).filter(daily_calories_needs__lt=CALORIE_LEVEL_HIGH)
+        if self.value() == self.FILTER_HIGH:
+            return queryset.filter(daily_calories_needs__gte=CALORIE_LEVEL_HIGH)
+
+
+class AgeFilter(admin.SimpleListFilter):
+    title = 'age'
+    parameter_name = 'age'
+    FILTER_LOW = f'>{AGE_TEEN}'
+    FILTER_MED = f'>{AGE_ADULT}'
+    FILTER_HIGH = f'>{AGE_OLD}'
+
+    today = date.today()
+
+    teen_min_birthdate = date(
+        today.year - AGE_ADULT, today.month, today.day
+    )
+    teen_max_birthdate = date(today.year - AGE_TEEN, today.month, today.day)
+
+    adult_min_birthdate = date(
+        today.year - AGE_OLD + 1, today.month, today.day
+    )
+    adult_max_birthdate = date(today.year - AGE_ADULT, today.month, today.day)
+
+    old_min_birthdate = date(today.year - AGE_OLD, today.month, today.day)
+
+    def lookups(self, request, model_admin):
+        return [
+            (self.FILTER_LOW, 'Teen'),
+            (self.FILTER_MED, 'Adult'),
+            (self.FILTER_HIGH, 'Old')
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == self.FILTER_LOW:
+            return queryset.filter(birthdate__range=(self.teen_min_birthdate, self.teen_max_birthdate))
+        if self.value() == self.FILTER_MED:
+            return queryset.filter(birthdate__range=(self.adult_min_birthdate, self.adult_max_birthdate))
+        if self.value() == self.FILTER_HIGH:
+            return queryset.filter(birthdate__lt=self.old_min_birthdate)
+
+
 @admin.register(models.Trainee)
 class TraineeAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['user']
+    exclude = ['daily_calories_intake',
+               'daily_water_intake', 'was_active_today']
+    actions = ['make_active', 'make_inactive', 'clear_streak']
+    list_display = ['name', 'username', 'age', 'height', 'weight',
+                    'calorie_level', 'activity_level', 'goal', 'was_active_today', 'streak',]
+    list_filter = [CalorieLevelFilter, AgeFilter,
+                   'activity_level', 'goal', 'was_active_today']
     list_select_related = ['user']
+    list_per_page = 100
+    ordering = ['user__first_name', 'user__last_name']
     search_fields = ['user__username__istartswith', 'user__first_name__istartswith',
                      'user__last_name__istartswith', 'user__email']
+
+    @admin.display(ordering='birthdate')
+    def name(self, trainee):
+        return trainee.full_name()
+
+    @admin.display(ordering='user__username')
+    def username(self, trainee):
+        url = (
+            reverse('admin:core_user_changelist') +
+            str(trainee.user.id)
+        )
+        return format_html('<a href="{}">{}</a>', url, trainee)
+
+    @admin.display(ordering='birthdate')
+    def age(self, trainee):
+        return trainee.age()
+
+    @admin.display(ordering='daily_calories_needs')
+    def calories_needs(self, trainee):
+        return trainee.daily_calories_needs
+
+    @admin.display(ordering='daily_calories_needs')
+    def calorie_level(self, trainee):
+        if trainee.daily_calories_needs < CALORIE_LEVEL_LOW:
+            return 'Low'
+        elif trainee.daily_calories_needs < CALORIE_LEVEL_HIGH:
+            return 'Medium'
+        return 'High'
+
+    @admin.display(ordering='daily_streak')
+    def streak(self, trainee):
+        return trainee.daily_streak
+
+    @admin.action(description='Make selected trainees active')
+    def make_active(self, request, queryset):
+        updated_count = queryset.update(was_active_today=True)
+        message = f'{updated_count} trainees were made active.'
+        self.message_user(request, message)
+
+    @admin.action(description='Make selected trainees inactive')
+    def make_inactive(self, request, queryset):
+        updated_count = queryset.update(was_active_today=False)
+        message = f'{updated_count} trainees were made inactive.'
+        self.message_user(request, message)
+
+    @admin.action(description='Clear streak of selected trainees')
+    def clear_streak(self, request, queryset):
+        updated_count = queryset.update(daily_streak=0)
+        message = f'{updated_count} trainees` streaks were cleared.'
+        self.message_user(request, message)
