9c919705990dfbc770d7f02dee739a60c0896a87
Mahmoud Saeed <mahmoudsaid246@gmail.com>
2023-04-01 03:17:05 +0200

    core models v3

--- core/models.py
+++ core/models.py
@@ -1,3 +1,5 @@
+from datetime import date
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -14,30 +16,46 @@ class Trainee(models.Model):
         HIGH = 'H', 'High'
         MEDIUM = 'M', 'Medium'
         LOW = 'L', 'Low'
+        TRACKED = 'T', 'Track your activity'
 
     class Goal(models.TextChoices):
-        GAIN = 'G', 'Gain'
-        KEEP = 'K', 'Keep'
-        LOSE = 'L', 'Lose'
+        GAIN = 'G', 'Gain weight'
+        KEEP = 'K', 'Keep weight'
+        LOSE = 'L', 'Lose weight'
 
     birthdate = models.DateField()
-    height = models.DecimalField(max_digits=4, decimal_places=1)
-    weight = models.DecimalField(max_digits=4, decimal_places=1)
-    daily_calories_needs = models.PositiveIntegerField()
-    daily_calories_intake = models.PositiveIntegerField()
-    daily_water_intake = models.PositiveIntegerField()
-    daily_water_needs = models.PositiveIntegerField()
-    carbs_ratio = models.DecimalField(max_digits=2, decimal_places=2)
-    fats_ratio = models.DecimalField(max_digits=2, decimal_places=2)
-    protein_ratio = models.DecimalField(max_digits=2, decimal_places=2)
-    was_active_today = models.BooleanField(default=False)
-    daily_streak = models.PositiveSmallIntegerField(default=0)
+    height = models.DecimalField(max_digits=4, decimal_places=1,
+                                 validators=[MinValueValidator(0)])
+    weight = models.DecimalField(max_digits=4, decimal_places=1,
+                                 validators=[MinValueValidator(0)])
+    # TODO: add defalut value
+    daily_calories_needs = models.PositiveIntegerField(blank=True)
+    daily_calories_intake = models.PositiveIntegerField(default=0, blank=True)
+    # TODO: add defalut value
+    daily_water_needs = models.PositiveIntegerField(blank=True)
+    daily_water_intake = models.PositiveIntegerField(default=0, blank=True)
+    # TODO: add defalut value
+    carbs_ratio = models.DecimalField(max_digits=2, decimal_places=2,
+                                      blank=True,
+                                      validators=[MinValueValidator(0), MaxValueValidator(1)])
+    # TODO: add defalut value
+    fats_ratio = models.DecimalField(max_digits=2, decimal_places=2,
+                                     blank=True,
+                                     validators=[MinValueValidator(0), MaxValueValidator(1)])
+    # TODO: add defalut value
+    protein_ratio = models.DecimalField(max_digits=2, decimal_places=2,
+                                        blank=True,
+                                        validators=[MinValueValidator(0), MaxValueValidator(1)])
+    was_active_today = models.BooleanField(default=False, blank=True)
+    daily_streak = models.PositiveSmallIntegerField(default=0, blank=True)
     activity_level = models.CharField(max_length=1,
                                       choices=ActivityLevel.choices,
-                                      default=ActivityLevel.MEDIUM)
+                                      default=ActivityLevel.MEDIUM,
+                                      blank=True)
     goal = models.CharField(max_length=1,
                             choices=Goal.choices,
-                            default=Goal.KEEP)
+                            default=Goal.KEEP,
+                            blank=True)
     image = models.ImageField(
         upload_to='core/images/trainees', null=True, blank=True
     )
@@ -45,3 +63,15 @@ class Trainee(models.Model):
 
     def __str__(self) -> str:
         return self.user.username
+
+    def age(self):
+        today = date.today()
+        dob = self.birthdate
+        years_difference = today.year - dob.year
+        is_before_birthday = (today.month, today.day) < (dob.month, dob.day)
+        if is_before_birthday:
+            years_difference -= 1
+        return years_difference
+
+    def full_name(self):
+        return self.user.first_name + ' ' + self.user.last_name
