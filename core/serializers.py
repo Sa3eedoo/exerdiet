9f3027d5d8ac48c2216e7630acf7afb6653eda17
Mahmoud Saeed <mahmoudsaid246@gmail.com>
2023-05-04 21:27:16 +0300

    update getting the current user

--- core/serializers.py
+++ core/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
+from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer, UserSerializer as BaseUserSerializer
 from .models import Trainee
 
 
@@ -7,6 +7,11 @@ class UserCreateSerializer(BaseUserCreateSerializer):
     class Meta(BaseUserCreateSerializer.Meta):
         fields = ['id', 'username', 'password',
                   'email', 'first_name', 'last_name']
+
+
+class UserSerializer(BaseUserSerializer):
+    class Meta(BaseUserSerializer.Meta):
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
 
 
 class TraineeSerializer(serializers.ModelSerializer):
