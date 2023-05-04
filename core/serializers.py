0859a19bd97886a061ff38c798308f720b131cb4
Mahmoud Saeed <mahmoudsaid246@gmail.com>
2023-05-04 18:57:46 +0300

    update user create serializer

--- core/serializers.py
+++ core/serializers.py
@@ -1,1 +1,8 @@
 from rest_framework import serializers
+from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
+
+
+class UserCreateSerializer(BaseUserCreateSerializer):
+    class Meta(BaseUserCreateSerializer.Meta):
+        fields = ['id', 'username', 'password',
+                  'email', 'first_name', 'last_name']
