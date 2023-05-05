28852d9677d87db7cfbf4c382558459bad7464aa
Mahmoud Saeed <mahmoudsaid246@gmail.com>
2023-05-05 23:20:24 +0300

    updating getting current trainee profile

--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import action
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, UpdateModelMixin
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.viewsets import GenericViewSet
 from .models import Trainee
 from .serializers import TraineeSerializer
@@ -12,15 +14,16 @@ class TraineeViewSet(CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, Gen
 
     @action(detail=False, methods=['GET', 'PUT', 'PATCH'])
     def me(self, request):
-        trainee, created = Trainee.objects.get_or_create(
-            user_id=request.user.id)
-        if request.method == 'GET':
-            serializer = TraineeSerializer(trainee)
-            return Response(serializer.data)
-        elif request.method == 'PUT' or request.method == 'PATCH':
-            partial = True if request.method == 'PATCH' else False
-            serializer = TraineeSerializer(
-                trainee, data=request.data, partial=partial)
-            serializer.is_valid(raise_exception=True)
-            serializer.save()
-            return Response(serializer.data)
+        if request.user.is_authenticated:
+            trainee = get_object_or_404(Trainee, user_id=request.user.id)
+            if request.method == 'GET':
+                serializer = TraineeSerializer(trainee)
+                return Response(serializer.data)
+            elif request.method == 'PUT' or request.method == 'PATCH':
+                partial = True if request.method == 'PATCH' else False
+                serializer = TraineeSerializer(
+                    trainee, data=request.data, partial=partial)
+                serializer.is_valid(raise_exception=True)
+                serializer.save()
+                return Response(serializer.data)
+        return Response(status=status.HTTP_401_UNAUTHORIZED, data={'detail': 'Authentication credentials were not provided.'})
