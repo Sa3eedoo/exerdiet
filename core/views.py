32b3f6cdebf48527dc0598c4785d59e15d9e46c7
Mahmoud Saeed <mahmoudsaid246@gmail.com>
2023-05-06 16:51:06 +0300

    update permissions for TraineeViewSet

--- core/views.py
+++ core/views.py
@@ -1,12 +1,10 @@
-from django.shortcuts import get_object_or_404
 from rest_framework.decorators import action
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, UpdateModelMixin
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework import status
 from rest_framework.viewsets import GenericViewSet
 from .models import Trainee
-from .permissions import IsAuthenticatedAndNotTrainee
+from .permissions import IsAuthenticatedAndNotTrainee, IsAuthenticatedAndTrainee
 from .serializers import TraineeSerializer
 
 
@@ -17,20 +15,20 @@ class TraineeViewSet(CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, Gen
     def get_permissions(self):
         if self.request.method == 'POST':
             return [IsAuthenticatedAndNotTrainee()]
+        if self.action == 'me':
+            return [IsAuthenticatedAndTrainee()]
         return [IsAuthenticated()]
 
     @action(detail=False, methods=['GET', 'PUT', 'PATCH'])
     def me(self, request):
-        if request.user.is_authenticated:
-            trainee = get_object_or_404(Trainee, user_id=request.user.id)
-            if request.method == 'GET':
-                serializer = TraineeSerializer(trainee)
-                return Response(serializer.data)
-            elif request.method == 'PUT' or request.method == 'PATCH':
-                partial = True if request.method == 'PATCH' else False
-                serializer = TraineeSerializer(
-                    trainee, data=request.data, partial=partial)
-                serializer.is_valid(raise_exception=True)
-                serializer.save()
-                return Response(serializer.data)
-        return Response(status=status.HTTP_401_UNAUTHORIZED, data={'detail': 'Authentication credentials were not provided.'})
+        trainee = Trainee.objects.get(user_id=request.user.id)
+        if request.method == 'GET':
+            serializer = TraineeSerializer(trainee)
+            return Response(serializer.data)
+        elif request.method == 'PUT' or request.method == 'PATCH':
+            partial = True if request.method == 'PATCH' else False
+            serializer = TraineeSerializer(
+                trainee, data=request.data, partial=partial)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
