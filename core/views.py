bbef522d0bc4046e6891fdcf6a1964efb9eaf15a
Mahmoud Saeed <mahmoudsaid246@gmail.com>
2023-05-06 23:47:26 +0300

    add delete to me action in TraineeViewSet

--- core/views.py
+++ core/views.py
@@ -1,14 +1,15 @@
 from rest_framework.decorators import action
-from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, UpdateModelMixin
+from rest_framework.mixins import CreateModelMixin
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
+from rest_framework import status
 from .models import Trainee
 from .permissions import IsAuthenticatedAndNotTrainee, IsAuthenticatedAndTrainee
 from .serializers import TraineeSerializer, TraineeCreateSerializer
 
 
-class TraineeViewSet(CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
+class TraineeViewSet(CreateModelMixin, GenericViewSet):
     queryset = Trainee.objects.all()
 
     def get_serializer_class(self):
@@ -26,16 +27,19 @@ class TraineeViewSet(CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, Gen
             return [IsAuthenticatedAndTrainee()]
         return [IsAuthenticated()]
 
-    @action(detail=False, methods=['GET', 'PUT', 'PATCH'])
+    @action(detail=False, methods=['GET', 'PATCH', 'DELETE'])
     def me(self, request):
         trainee = Trainee.objects.get(user_id=request.user.id)
         if request.method == 'GET':
             serializer = TraineeSerializer(trainee)
             return Response(serializer.data)
-        elif request.method == 'PUT' or request.method == 'PATCH':
+        elif request.method == 'PATCH':
             partial = True if request.method == 'PATCH' else False
             serializer = TraineeSerializer(
                 trainee, data=request.data, partial=partial)
             serializer.is_valid(raise_exception=True)
             serializer.save()
             return Response(serializer.data)
+        elif request.method == 'DELETE':
+            trainee.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
