6f84a802c5b1379fbfba550cbbd2d6f13ca03be2
ReemAbdelhaleemHassan <102296115+ReemAbdelhaleemHassan@users.noreply.github.com>
2023-05-07 21:55:20 +0300

    gym views v2.1
    custom exercise added

--- gym/views.py
+++ gym/views.py
@@ -3,9 +3,9 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import ExerciseSerializer
-from .models import Exercise
-from .filters import ExerciseFilter
+from .models import Exercise, CustomExercise
+from .serializers import ExerciseSerializer, CustomExerciseSerializer
+from .filters import ExerciseFilter, CustomExerciseFilter
 from .pagination import DefaultPagination
 
 
@@ -21,3 +21,17 @@ class ExerciseViewSet(viewsets.ModelViewSet):
         return {'request': self.request}
     # search_fields
     # ordering_fields
+    
+    
+class CustomExerciseViewSet(viewsets.ModelViewSet):
+    queryset = CustomExercise.objects.all()
+    serializer_class = CustomExerciseSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_class = CustomExerciseFilter
+    pagination_class = DefaultPagination
+    
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance.exercise_instances.exists():
+            return Response({'error': 'Exercise cannot be deleted because it is associated with a workout.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        return Response(status=status.HTTP_204_NO_CONTENT)
