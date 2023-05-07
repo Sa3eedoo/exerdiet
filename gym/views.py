32bf75edbecd0d910489ac6496ca37eb663d9814
ReemAbdelhaleemHassan <102296115+ReemAbdelhaleemHassan@users.noreply.github.com>
2023-05-07 22:38:33 +0300

    gym views v2.3
    performed workout viewset is added

--- gym/views.py
+++ gym/views.py
@@ -3,9 +3,9 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Exercise, CustomExercise, Workout
-from .serializers import ExerciseSerializer, CustomExerciseSerializer, WorkoutSerializer
-from .filters import ExerciseFilter, CustomExerciseFilter, WorkoutFilter
+from .models import Exercise, CustomExercise, Workout, PerformedWorkout
+from .serializers import ExerciseSerializer, CustomExerciseSerializer, WorkoutSerializer, PerformedWorkoutSerializer
+from .filters import ExerciseFilter, CustomExerciseFilter, WorkoutFilter, PerformedWorkoutFilter
 from .pagination import DefaultPagination
 
 
@@ -50,3 +50,11 @@ class WorkoutViewSet(viewsets.ModelViewSet):
             return Response({'error': 'Workout cannot be deleted because it is associated with a performed workout.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
         self.perform_destroy(instance)
         return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    
+class PerformedWorkoutViewSet(viewsets.ModelViewSet):
+    queryset = PerformedWorkout.objects.all()
+    serializer_class = PerformedWorkoutSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_class = PerformedWorkoutFilter
+    pagination_class = DefaultPagination
