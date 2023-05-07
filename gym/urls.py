79ce91471f51ba7742539f711540c4edac59a8c5
ReemAbdelhaleemHassan <102296115+ReemAbdelhaleemHassan@users.noreply.github.com>
2023-05-07 22:38:07 +0300

    gym urls v1.3
    performed workout url is added

--- gym/urls.py
+++ gym/urls.py
@@ -5,6 +5,7 @@ router = routers.DefaultRouter()
 router.register('exercises', views.ExerciseViewSet, basename='exercises')
 router.register('custom_exercises', views.CustomExerciseViewSet, basename='custom_exercises')
 router.register('workouts', views.WorkoutViewSet, basename='workouts')
+router.register('performed_workouts', views.PerformedWorkoutViewSet, basename='performed_workouts')
 
 exercises_router = routers.NestedDefaultRouter(
     router, 'exercises', lookup='exercise')
@@ -15,5 +16,8 @@ custom_exercises_router = routers.NestedDefaultRouter(
 workouts_router = routers.NestedDefaultRouter(
     router, 'workouts', lookup='workout')
 
+performed_workouts_router = routers.NestedDefaultRouter(
+    router, 'performed_workouts', lookup='performed_workout')
+
 # URLConf
-urlpatterns = router.urls + exercises_router.urls + custom_exercises_router.urls
+urlpatterns = router.urls + exercises_router.urls + custom_exercises_router.urls + workouts_router.urls + performed_workouts_router.urls
