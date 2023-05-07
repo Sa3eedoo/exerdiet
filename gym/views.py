d6f40d8aa92d708da0f8fa6efb33de7be0fc64a4
ReemAbdelhaleemHassan <102296115+ReemAbdelhaleemHassan@users.noreply.github.com>
2023-05-07 21:42:17 +0300

    gym views v2.0

--- gym/views.py
+++ gym/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import ExerciseSerializer
+from .models import Exercise
+from .filters import ExerciseFilter
+from .pagination import DefaultPagination
 
-# Create your views here.
+
+class ExerciseViewSet(viewsets.ModelViewSet):
+    queryset = Exercise.objects.all()
+    serializer_class = ExerciseSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_class = ExerciseFilter
+    pagination_class = DefaultPagination
+    http_method_names = ['get', 'head']
+
+    def get_serializer_context(self):
+        return {'request': self.request}
+    # search_fields
+    # ordering_fields
