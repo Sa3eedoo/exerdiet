8f2a88a8ed595af67a2e3e0411f91ddef38874ef
ReemAbdelhaleemHassan <102296115+ReemAbdelhaleemHassan@users.noreply.github.com>
2023-05-07 21:55:00 +0300

    gym urls v1.1
    custom exercise added

--- gym/urls.py
+++ gym/urls.py
@@ -1,13 +1,15 @@
-from django.urls import path
-from django.urls.conf import include
 from rest_framework_nested import routers
 from . import views
 
 router = routers.DefaultRouter()
 router.register('exercises', views.ExerciseViewSet, basename='exercises')
+router.register('custom_exercises', views.CustomExerciseViewSet, basename='custom_exercises')
 
 exercises_router = routers.NestedDefaultRouter(
     router, 'exercises', lookup='exercise')
 
+custom_exercises_router = routers.NestedDefaultRouter(
+    router, 'custom_exercises', lookup='custom_exercise')
+
 # URLConf
-urlpatterns = router.urls + exercises_router.urls
+urlpatterns = router.urls + exercises_router.urls + custom_exercises_router.urls
