aae859722aeb709c16778af64f58ad15dcc580ad
Mahmoud Saeed <mahmoudsaid246@gmail.com>
2023-05-09 02:47:51 +0300

    update prod settings

--- exerdiet/settings/prod.py
+++ exerdiet/settings/prod.py
@@ -5,4 +5,14 @@ DEBUG = False
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['exerdiet.pythonanywhere.com']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'exerdiet$exerdiet',
+        'USER': 'exerdiet',
+        'PASSWORD': os.environ['DATABASE_PASSWORD'],
+        'HOST': 'exerdiet.mysql.pythonanywhere-services.com',
+    }
+}
