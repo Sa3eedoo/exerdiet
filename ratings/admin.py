4eb351a5ebe5967906238162ec8d79cdf65fb623
ReemAbdelhaleemHassan <102296115+ReemAbdelhaleemHassan@users.noreply.github.com>
2023-05-16 21:43:00 +0300

    ratings admin

--- ratings/admin.py
+++ ratings/admin.py
@@ -4,6 +4,8 @@ from .models import Rating
 
 class RatingAdmin(admin.ModelAdmin):
     raw_id_fields = ['trainee']
+    list_display = ['content_object', 'trainee', 'value', 'active']
+    search_fields = ['trainee__username']
     readonly_fields = ['content_object']
 
 admin.site.register(Rating, RatingAdmin)
