b10510e867054bc5d67de255d4a991470b9dd34b
ReemAbdelhaleemHassan <102296115+ReemAbdelhaleemHassan@users.noreply.github.com>
2023-05-16 20:40:08 +0300

    trainee not user

--- ratings/models.py
+++ ratings/models.py
@@ -3,16 +3,8 @@ from django.db import models
 from django.db.models import Avg
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
+from core.models import Trainee, User
 
-
-User = settings.AUTH_USER_MODEL # 'auth.User'
-
-# user_obj = User.objects.first()
-# user_ratings = user_obj.rating_set.all()
-
-# rating_obj = Rating.objects.first()
-# user_obj = rating_obj.user
-# user_ratings = user_obj.rating_set.all()
 
 class RatingChoice(models.IntegerChoices):
     ONE = 1 
@@ -34,7 +26,7 @@ class RatingManager(models.Manager):
         return self.get_queryset().avg()
 
 class Rating(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    trainee = models.ForeignKey(Trainee, on_delete=models.CASCADE, related_name='ratings', null=True, blank=True)
     value = models.IntegerField(null=True, blank=True, choices=RatingChoice.choices)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
