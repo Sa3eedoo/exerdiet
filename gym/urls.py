5b023fbeb14613a679fb821839e6ac8f57231fa4
Mahmoud Saeed <mahmoudsaid246@gmail.com>
2023-05-16 03:29:29 +0300

    add nested router for exercise_instances to workouts

--- gym/urls.py
+++ gym/urls.py
@@ -3,17 +3,20 @@ from . import views
 
 router = routers.DefaultRouter()
 router.register('exercises', views.ExerciseViewSet)
-
 router.register('custom_exercises',
                 views.CustomExerciseViewSet,
                 basename='custom_exercises')
-
 router.register('workouts', views.WorkoutViewSet, basename='workouts')
-
 router.register('performed_workouts',
                 views.PerformedWorkoutViewSet,
                 basename='performed_workouts')
 
+workouts_router = routers.NestedDefaultRouter(router,
+                                              'workouts',
+                                              lookup='workout')
+workouts_router.register('exercise_instances',
+                         views.WorkoutExerciseInstanceViewSet,
+                         basename='workouts-exercise_instances')
 
 # URLConf
-urlpatterns = router.urls
+urlpatterns = router.urls + workouts_router.urls
