7c0cb2da127e92b449c8802091d572383b7c21a1
ReemAbdelhaleemHassan <102296115+ReemAbdelhaleemHassan@users.noreply.github.com>
2023-05-16 20:39:31 +0300

    use trainee not user

--- ratings/admin.py
+++ ratings/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Rating 
 
 class RatingAdmin(admin.ModelAdmin):
-    raw_id_fields = ['user']
+    raw_id_fields = ['trainee']
     readonly_fields = ['content_object']
 
 admin.site.register(Rating, RatingAdmin)
