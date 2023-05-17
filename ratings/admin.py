1e783405a5cdfd0d54c3609969a43c69da386887
ReemAbdelhaleemHassan <102296115+ReemAbdelhaleemHassan@users.noreply.github.com>
2023-05-18 00:58:26 +0300

    ratings admin add active and value filter

--- ratings/admin.py
+++ ratings/admin.py
@@ -4,8 +4,9 @@ from .models import Rating
 
 class RatingAdmin(admin.ModelAdmin):
     raw_id_fields = ['trainee']
-    list_display = ['content_object', 'trainee', 'value', 'active']
-    search_fields = ['trainee__username']
+    list_display = ['content_object', 'trainee', 'value', 'active', 'content_type']
+    search_fields = ['trainee__user__username']
     readonly_fields = ['content_object']
+    list_filter = ['active', 'value']
 
 admin.site.register(Rating, RatingAdmin)
