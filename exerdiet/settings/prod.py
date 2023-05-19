672e7e0a13912d9ed591a4c72e6fcede5155c7a8
Mahmoud Saeed <mahmoudsaid246@gmail.com>
2023-05-20 00:03:55 +0300

    update lifetime of access token for prod

--- exerdiet/settings/prod.py
+++ exerdiet/settings/prod.py
@@ -23,7 +23,7 @@ DATABASES = {
 
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'ROTATE_REFRESH_TOKENS': True
 }
