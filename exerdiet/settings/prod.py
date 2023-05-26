6a3701d5dc7216112f66a0aa35b4c130352ae72d
Mahmoud Saeed <mahmoudsaid246@gmail.com>
2023-05-26 03:06:32 +0300

    configure email settings for dev and prod

--- exerdiet/settings/prod.py
+++ exerdiet/settings/prod.py
@@ -27,3 +27,9 @@ SIMPLE_JWT = {
     'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'ROTATE_REFRESH_TOKENS': True
 }
+
+EMAIL_HOST = os.environ['EMAIL_HOST']
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+EMAIL_PORT = os.environ['EMAIL_PORT']
+EMAIL_USE_SSL = True
