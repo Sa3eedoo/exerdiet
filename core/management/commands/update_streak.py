2977b153a1457ce6762100e182121ab12faed9c6
Mahmoud Saeed <mahmoudsaid246@gmail.com>
2023-06-27 17:24:30 +0300

    create custom command to update streak

---
+++ core/management/commands/update_streak.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from django.db import transaction
+from core.models import Trainee
+
+
+class Command(BaseCommand):
+    help = 'Updates streak of all trainees'
+
+    def handle(self, *args, **options):
+        print('updating streak of all trainees...')
+        with transaction.atomic():
+            for trainee in Trainee.objects.all():
+                if trainee.was_active_today:
+                    trainee.daily_streak += 1
+                trainee.was_active_today = False
+                trainee.save()
