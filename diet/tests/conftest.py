2fe7710c2f50fecdb5d8bfa425b75c0382ab85c1
Mahmoud Saeed <mahmoudsaid246@gmail.com>
2023-06-29 19:26:20 +0300

    create diet conftest

---
+++ diet/tests/conftest.py
@@ -0,0 +1,24 @@
+from model_bakery import baker
+from rest_framework.test import APIClient
+import pytest
+from core.models import User, Trainee
+
+
+@pytest.fixture
+def api_client():
+    return APIClient()
+
+
+@pytest.fixture
+def authenticate_without_trainee(api_client):
+    def do_authenticate_without_trainee(is_staff=False):
+        return api_client.force_authenticate(user=User(is_staff=is_staff))
+    return do_authenticate_without_trainee
+
+
+@pytest.fixture
+def authenticate_with_trainee(api_client):
+    def do_authenticate_with_trainee(is_staff=False):
+        trainee = baker.make(Trainee)
+        return api_client.force_authenticate(user=trainee.user)
+    return do_authenticate_with_trainee
