8297d4b97020035e77f0bbdd11c84293c950f7ba
Mahmoud Saeed <mahmoudsaid246@gmail.com>
2023-06-29 20:26:44 +0300

    add TestRetrieveFood to test_foods

--- diet/tests/test_foods.py
+++ diet/tests/test_foods.py
@@ -1,6 +1,7 @@
+from model_bakery import baker
 from rest_framework import status
 import pytest
-from core.models import User
+from diet.models import Food
 
 
 @pytest.fixture
@@ -10,16 +11,51 @@ def create_food(api_client):
     return do_create_food
 
 
-@ pytest.mark.django_db
+@pytest.fixture
+def retrieve_food(api_client):
+    def do_retrieve_food(id):
+        return api_client.get(f'/diet/foods/{id}/')
+    return do_retrieve_food
+
+
+@pytest.mark.django_db
 class TestCreateFood:
-    def test_if_user_is_anonymous_retruns_401(self, create_food):
+    def test_if_user_is_anonymous_returns_401(self, create_food):
         response = create_food({})
 
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
-    def test_if_user_is_authenticated_retruns_405(self, authenticate_with_trainee, create_food):
+    def test_if_user_is_authenticated_returns_405(self, authenticate_with_trainee, create_food):
         authenticate_with_trainee()
 
         response = create_food({})
 
         assert response.status_code == status.HTTP_405_METHOD_NOT_ALLOWED
+
+
+@pytest.mark.django_db
+class TestRetrieveFood:
+    def test_if_food_Not_exists_returns_404(self, retrieve_food, authenticate_with_trainee):
+        authenticate_with_trainee()
+
+        response = retrieve_food(1)
+
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+    def test_if_food_exists_returns_200(self, retrieve_food, authenticate_with_trainee):
+        authenticate_with_trainee()
+        food = baker.make(Food)
+
+        response = retrieve_food(food.id)
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == {
+            "id": food.id,
+            "name": food.name,
+            "category": food.category,
+            "calories": food.calories,
+            "carbs": food.carbs,
+            "fats": food.fats,
+            "protein": food.protein,
+            "image": food.image
+        }
